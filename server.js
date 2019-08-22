adb6f2b4081d8540b5734c1c6c2f5c0d358cf9ff
madhanraj.r <madhanraj.r@mnw.co.in>
2019-08-22 18:05:06 +0530

    new update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/admin'));
 
 app.get('/*', function(req,res) {
-    
+ res.setMaxListeners(0);   
 res.sendFile(path.join(__dirname+'/dist/admin/index.html'));
 });
 
